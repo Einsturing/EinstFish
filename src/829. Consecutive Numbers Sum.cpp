543364e5c86707b14bb32edaf53da8ec5bf0207d
Einsturing <1106420198@qq.com>
2022-06-03 11:53:11 +0800

    2022/6/3

---
+++ src/829. Consecutive Numbers Sum.cpp
@@ -0,0 +1,22 @@
+//难度-困难
+//数学题
+class Solution
+{
+public:
+    int consecutiveNumbersSum(int n)
+    {
+        int ans = 0;
+        int maxn = 2 * n;
+        for (int k = 1; k * (k + 1) <= maxn; k++)
+        {
+            if (k % 2 == 1)
+            {
+                if (n % k == 0)
+                    ans++;
+            }
+            else if (n % k != 0 && (2 * n) % k == 0)
+                ans++;
+        }
+        return ans;
+    }
+};
