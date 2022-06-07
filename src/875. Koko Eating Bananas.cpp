1d648bbef80fc49aa766d6b013f034e7122088e3
Einsturing <1106420198@qq.com>
2022-06-07 11:02:29 +0800

    2022/6/7

---
+++ src/875. Koko Eating Bananas.cpp
@@ -0,0 +1,33 @@
+//难度-中等
+//二分查找
+class Solution
+{
+public:
+    int minEatingSpeed(vector<int> &piles, int h)
+    {
+        int left = 1, right = 0;
+        for (auto p : piles)
+            right = max(right, p);
+        int k = right;
+        while (left < right)
+        {
+            int speed = (right - left) / 2 + left;
+            int time = getTime(speed, piles);
+            if (time <= h)
+            {
+                k = speed;
+                right = speed;
+            }
+            else
+                left = speed + 1;
+        }
+        return k;
+    }
+    int getTime(int speed, vector<int> &piles)
+    {
+        int time = 0;
+        for (auto p : piles)
+            time += (p + speed - 1) / speed;
+        return time;
+    }
+};
