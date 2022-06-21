6b940b12fdd6c2ca8c9956e2ed5e212402265ec2
Einsturing <1106420198@qq.com>
2022-06-21 12:12:33 +0800

    2022/6/21

---
+++ src/1108. Defanging an IP Address.cpp
@@ -0,0 +1,13 @@
+//难度-简单
+//？
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string res;
+        for(int i = 0; i < address.size(); i++) {
+            if(address[i] == '.') res += "[.]";
+            else res += address[i];
+        }
+        return res;
+    }
+};
