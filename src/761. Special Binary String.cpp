6bb1a4fd3e3a2f7f8effb1d8a85411de3e61d370
Einsturing <1106420198@qq.com>
2022-08-08 09:53:13 +0800

    2022/8/8

---
+++ src/761. Special Binary String.cpp
@@ -0,0 +1,30 @@
+//难度-困难
+//递归
+class Solution
+{
+public:
+    string makeLargestSpecial(string s)
+    {
+        if (s.size() <= 2)
+            return s;
+        int cnt = 0, left = 0;
+        vector<string> subs;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == '1')
+                cnt++;
+            else
+            {
+                cnt--;
+                if (cnt == 0)
+                {
+                    subs.push_back("1" + makeLargestSpecial(s.substr(left + 1, i - left - 1)) + "0");
+                    left = i + 1;
+                }
+            }
+        }
+        sort(subs.begin(), subs.end(), greater<string>());
+        string res = accumulate(subs.begin(), subs.end(), ""s);
+        return res;
+    }
+};
