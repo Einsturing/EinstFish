8d34b009660f57d11a9c55545b7ccf3ea13ce711
Einsturing <1106420198@qq.com>
2022-08-26 10:10:00 +0800

    2022/8/26

---
+++ src/1464. Maximum Product of Two Elements in an Array.cpp
@@ -0,0 +1,12 @@
+//难度-简单
+//排序
+class Solution
+{
+public:
+    int maxProduct(vector<int> &nums)
+    {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        return (nums[n - 1] - 1) * (nums[n - 2] - 1);
+    }
+};
