5151a05baa2cdedd071057505c215cedf87d168b
einsturing <1106420198@qq.com>
2023-07-06 11:21:05 +0800

    work update

---
+++ src/forOffer/2178.拆分成最多数目的正偶数之和.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=2178 lang=cpp
+ *
+ * [2178] 拆分成最多数目的正偶数之和
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<long long> maximumEvenSplit(long long finalSum) {
+        
+    }
+};
+// @lc code=end
+
